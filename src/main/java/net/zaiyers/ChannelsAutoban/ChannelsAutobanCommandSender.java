a0cd3f260396021ddde18ea9e29fc7db8a9e0b43
Max Lee <Phoenix616@users.noreply.github.com>
2015-03-22 03:20:37 +0100

    Log messages a custom command sender receivers

--- src/main/java/net/zaiyers/ChannelsAutoban/ChannelsAutobanCommandSender.java
+++ src/main/java/net/zaiyers/ChannelsAutoban/ChannelsAutobanCommandSender.java
@@ -3,6 +3,7 @@ package net.zaiyers.ChannelsAutoban;
 import java.util.Collection;
 
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.BaseComponent;
 
 public class ChannelsAutobanCommandSender implements CommandSender {
@@ -21,9 +22,21 @@ public class ChannelsAutobanCommandSender implements CommandSender {
 	public Collection<String> getPermissions() { return null; }
 	public boolean hasPermission(String arg0) {	return true; }
 	public void removeGroups(String... arg0) {}
-	public void sendMessage(String arg0) {}
-	public void sendMessage(BaseComponent... arg0) {}
-	public void sendMessage(BaseComponent arg0) {}
-	public void sendMessages(String... arg0) {}
+	public void sendMessage(String arg0) {
+        ProxyServer.getInstance().getLogger().info(name + ": " + arg0);
+    }
+	public void sendMessage(BaseComponent... arg0) {
+        for(BaseComponent b : arg0) {
+            ProxyServer.getInstance().getLogger().info(name + ": " + b.toPlainText());
+        }
+    }
+	public void sendMessage(BaseComponent arg0) {
+        ProxyServer.getInstance().getLogger().info(name + ": " + arg0.toPlainText());
+    }
+	public void sendMessages(String... arg0) {
+        for(String s : arg0) {
+            ProxyServer.getInstance().getLogger().info(name + ": " + s);
+        }
+    }
 	public void setPermission(String arg0, boolean arg1) {}
 }
